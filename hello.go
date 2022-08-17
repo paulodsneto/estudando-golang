1425171f8b18d83c2091d595a62d9ad460ba4362
Paulo Neto <paulodsneto@outlook.com>
2022-08-17 17:05:09 -0300

    Separando o programa em func's

--- hello.go
+++ hello.go
@@ -2,18 +2,46 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Comando inválido.")
+		os.Exit(-1)
+	}
+
+}
+
+func exibeIntroducao() {
 	nome := "Paulo"
 	versao := 1.1
 	fmt.Println("Olá senhor", nome, "esse programa está na versão", versao)
+}
 
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi: ", comandoLido)
+
+	return comandoLido
+}
+
+func exibeMenu() {
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do Programa")
-
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O programa escolhido foi: ", comando)
 }
